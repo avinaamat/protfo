0663e1ddc9cf5a578698a5289110882f516f363f
Aviad Naamat <avinaamat@gmail.com>
2021-01-27 01:59:40 +0200

    update to a workable site

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def submit_form():
                 del data['submit']
             write_to_txt(data)
             write_to_csv(data)
-            return 'adsgfdfg'
+            return "I'll be in touch soon."
         except:
             return 'did not save to database'
         # return redirect('/thankyou.HTML')
